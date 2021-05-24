df4df0203a058699917fe44cb8017a8db838f236
jonguz6 <jonguz6@wp.pl>
2021-05-24 23:05:48 +0200

    test: make test for string_part_reversing function

--- test_main.py
+++ test_main.py
@@ -1,9 +1,14 @@
 from unittest.case import TestCase
 
-from main import hex_string_reverse
+from main import hex_string_reverse, hex_part_reverse
 
 
 class SnowrunnerTestCase(TestCase):
     def test_hex_string_reverse(self):
         self.assertEqual(hex_string_reverse('B1DC9D9F 96E1 7945 B3F1 4F 3D 51 71 B5 61'),
-                         '9F9DDCB1 E196 4579 F1B3 4F 3D 51 71 B5 61')
+                         '9F9DDCB1E1964579F1B34F3D5171B561')
+
+    def test_hex_part_reverse(self):
+        self.assertEqual(hex_part_reverse('B1DC9D9F'), '9F9DDCB1')
+        self.assertEqual(hex_part_reverse('96E1'), 'E196')
+        self.assertEqual(hex_part_reverse('7945'), '4579')
