6a006ac16a718178362ba759eb032f5c1df62559
jonguz6 <jonguz6@wp.pl>
2021-05-24 23:46:13 +0200

    test: test function for parsing filename from hex

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 from unittest.case import TestCase
 
-from main import hex_string_reverse, hex_part_reverse
+from main import hex_string_reverse, hex_part_reverse, filename_parser
 
 
 class SnowrunnerTestCase(TestCase):
@@ -12,3 +12,8 @@ class SnowrunnerTestCase(TestCase):
         self.assertEqual(hex_part_reverse('B1DC9D9F'), '9F9DDCB1')
         self.assertEqual(hex_part_reverse('96E1'), 'E196')
         self.assertEqual(hex_part_reverse('7945'), '4579')
+
+    def test_filename_parser(self):
+        self.assertEqual(filename_parser('''43006F006D0070006C006500740065005300610076006500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004D20A19BF0F4D44A03B6BC8E7CC248504D20A19BF0F4D44A03B6BC8E7CC2485'''),
+                         {'Complete Save': '190AD2040FBF444D3BA06BC8E7CC2485'})
+
