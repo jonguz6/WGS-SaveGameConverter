2c6d877ad13ca49cb38ee39f88879f472f9f8750
jonguz6 <jonguz6@wp.pl>
2021-05-25 00:49:04 +0200

    feature: introduce filename parser

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def hex_part_reverse(hex_part: str, split_length: int = 2) -> str:
-    hex_part = [hex_part[i:i+split_length] for i in range(0, len(hex_part), split_length)]
+    hex_part = [hex_part[i:i + split_length] for i in range(0, len(hex_part), split_length)]
     hex_part.reverse()
     hex_part_reversed = ''
     for item in hex_part:
@@ -22,4 +22,12 @@ def hex_string_reverse(hex_string: str) -> str:
 
     return int32 + int16_1 + int16_2 + int16_3 + hex_string[20:]
 
+
+def filename_parser(data: str) -> dict:
+    data_array = data.split('''000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000''')
+    wgs_filename = data_array[1][:32]
+    wgs_filename = hex_string_reverse(wgs_filename)
+    steam_filename = bytes.fromhex(data_array[0]).decode().replace('\x00', '')
+    data_dict = {steam_filename: wgs_filename}
+    return data_dict
 
